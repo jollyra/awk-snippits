8b99a2fecba84c1f5858d0873ac3e2cdaa06c9a1
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2016-03-05 15:30:34 -0800

    Codify the stockfighter get api calls

--- stockfighter.js
+++ stockfighter.js
@@ -23,55 +23,21 @@ function heartbeat() {
   return get('https://api.stockfighter.io/ob/api/venues/TESTEX/heartbeat');
 }
 
-function listStocksByEx(venue) {
-  https.get(`https://api.stockfighter.io/ob/api/venues/${venue}/stocks`, function (res) {
-
-    res.on('data', function (d) {
-      process.stdout.write(d);
-    });
-
-  }).on('error', function (e) {
-    console.error(e);
-  });
+var api = {
+  hearbeat: function () {
+    'https://api.stockfighter.io/ob/api/venues/TESTEX/heartbeat'
+  },
+  listStocksByVenue: function (venue) {
+    return `https://api.stockfighter.io/ob/api/venues/${venue}/stocks`;
+  },
+  getQuote: function (venue, stock) {
+    return `http https://api.stockfighter.io/ob/api/venues/${venue}/stocks/${stock}/quote`;
+  },
+  getOrderbook: function (venue, stock) {
+    return `https://api.stockfighter.io/ob/api/venues/${venue}/stocks/${stock}`;
+  }
 }
 
-function getQuote(venue, stock) {
-  https.get(`http https://api.stockfighter.io/ob/api/venues/${venue}/stocks/${stock}/quote`, function (res) {
-
-    res.on('data', function (d) {
-      process.stdout.write(d);
-    });
-
-  }).on('error', function (e) {
-    console.error(e);
-  });
-}
-
-function getOrderbook(venue, stock, callback) {
-  https.get(`https://api.stockfighter.io/ob/api/venues/${venue}/stocks/${stock}`, function (res) {
-    var body = "";
-
-    res.on('data', function (d) {
-      body += d;
-    });
-
-    res.on('end', function () {
-      callback(null, body);
-    });
-
-  }).on('error', function (e) {
-    callback(e, null);
-  });
-}
-
-// getOrderbook('TESTEX', 'FOOBAR', function (err, data) {
-//   if (err) {
-//     throw 'The world is burning!' + err;
-//   }
-//   var bids = data.bids;
-//   console.log(data);
-// });
-
-heartbeat().then(function (res) {
+get(api.getOrderbook('TESTEX', 'FOOBAR')).then(function (res) {
   console.log(res);
 });
