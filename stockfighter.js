eda3bdb6763f032547763510fb7c8183b2af0295
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2016-03-05 15:21:55 -0800

    Promisify https.get

--- stockfighter.js
+++ stockfighter.js
@@ -1,7 +1,30 @@
 var https = require('https');
 
+function get(url) {
+  return new Promise(function (resolve, reject) {
+    https.get(url, function (res) {
+      var body = "";
+
+      res.on('data', function (d) {
+        body += d;
+      });
+
+      res.on('end', function () {
+        resolve(body);
+      });
+
+    }).on('error', function (e) {
+      reject(e);
+    });
+  });
+}
+
 function heartbeat() {
-  https.get('https://api.stockfighter.io/ob/api/venues/TESTEX/heartbeat', function (res) {
+  return get('https://api.stockfighter.io/ob/api/venues/TESTEX/heartbeat');
+}
+
+function listStocksByEx(venue) {
+  https.get(`https://api.stockfighter.io/ob/api/venues/${venue}/stocks`, function (res) {
 
     res.on('data', function (d) {
       process.stdout.write(d);
@@ -12,8 +35,8 @@ function heartbeat() {
   });
 }
 
-function listStocksByEx(venue) {
-  https.get(`https://api.stockfighter.io/ob/api/venues/${venue}/stocks`, function (res) {
+function getQuote(venue, stock) {
+  https.get(`http https://api.stockfighter.io/ob/api/venues/${venue}/stocks/${stock}/quote`, function (res) {
 
     res.on('data', function (d) {
       process.stdout.write(d);
@@ -41,10 +64,14 @@ function getOrderbook(venue, stock, callback) {
   });
 }
 
-getOrderbook('TESTEX', 'FOOBAR', function (err, data) {
-  if (err) {
-    throw 'The world is burning!' + err;
-  }
-  var bids = data.bids;
-  console.log(data);
+// getOrderbook('TESTEX', 'FOOBAR', function (err, data) {
+//   if (err) {
+//     throw 'The world is burning!' + err;
+//   }
+//   var bids = data.bids;
+//   console.log(data);
+// });
+
+heartbeat().then(function (res) {
+  console.log(res);
 });
