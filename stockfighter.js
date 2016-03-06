26e8dc1fd13bc2936f91c8601b75d870b4c9c596
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2016-03-05 16:39:58 -0800

    Promisify placeOrder api post

--- stockfighter.js
+++ stockfighter.js
@@ -20,37 +20,42 @@ function get(url) {
 }
 
 function placeOrder(order) {
+  return new Promise(function (resolve, reject) {
 
-  var options = {
-    hostname: 'api.stockfighter.io',
-    path: `/ob/api/venues/${order.venue}/stocks/${order.stock}/orders`,
-    method: 'POST',
-    agent: false,
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Starfighter-Authorization': '40f908637aba1d813c19119ff50aa1b20e0e925a'
-    }
-  };
+    var options = {
+      hostname: 'api.stockfighter.io',
+      path: `/ob/api/venues/${order.venue}/stocks/${order.stock}/orders`,
+      method: 'POST',
+      agent: false,
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Starfighter-Authorization': '40f908637aba1d813c19119ff50aa1b20e0e925a'
+      }
+    };
 
-  var req = https.request(options, (res) => {
-    console.log(`STATUS: ${res.statusCode}`);
-    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-    res.setEncoding('utf8');
-    res.on('data', (chunk) => {
-      console.log(`BODY: ${chunk}`);
+    var req = https.request(options, (res) => {
+      console.log(`STATUS: ${res.statusCode}`);
+      // console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+      //
+      res.setEncoding('utf8');
+      var body = "";
+      res.on('data', (chunk) => {
+        body += chunk;
+      });
+
+      res.on('end', () => {
+        resolve(body);
+      });
     });
-    res.on('end', () => {
-      console.log('No more data in response.')
-    })
-  });
 
-  req.on('error', (e) => {
-    console.log(`problem with request: ${e.message}`);
-  });
+    req.on('error', (e) => {
+      reject(e);
+    });
 
-  // write data to request body
-  req.write(JSON.stringify(order));
-  req.end();
+    // write data to request body
+    req.write(JSON.stringify(order));
+    req.end();
+  });
 }
 
 var api = {
@@ -81,7 +86,6 @@ var order = {
   'orderType': 'limit'
 }
 
-// placeOrder('TESTEX', 'FOOBAR', order).then(function (res) {
-//   console.log(res);
-// });
-placeOrder(order);
+placeOrder(order).then(function (res) {
+  console.log(res);
+});
