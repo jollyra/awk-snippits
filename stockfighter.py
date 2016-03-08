9aedc9bb7d630fceb9e2f0124ce71ac3b8e54929
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2016-03-07 23:17:02 -0800

    List stocks for a venue

--- stockfighter.py
+++ stockfighter.py
@@ -5,5 +5,8 @@ def heartbeat(venue):
   r = requests.get('https://api.stockfighter.io/ob/api/venues/%s/heartbeat' % venue)
   print('%s is online' % r.json()['venue'])
 
-heartbeat('TESTEX')
+def stocks(venue):
+  r = requests.get('https://api.stockfighter.io/ob/api/venues/%s/stocks' % venue)
+  return r.json()
 
+print(stocks('TESTEX'))
