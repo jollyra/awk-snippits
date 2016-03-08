b207724ea83cd54dcbe8fad01fb67b857eaaedfc
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2016-03-07 23:41:35 -0800

    Place an order for a stock

--- stockfighter.py
+++ stockfighter.py
@@ -1,5 +1,8 @@
 import requests
+import json
 
+
+apikey = '40f908637aba1d813c19119ff50aa1b20e0e925a'
 
 def heartbeat(venue):
   r = requests.get('https://api.stockfighter.io/ob/api/venues/%s/heartbeat' % venue)
@@ -15,5 +18,32 @@ def orderbook(venue, symbol):
   r = requests.get('https://api.stockfighter.io/ob/api/venues/%s/stocks/%s' % (venue, symbol))
   orderbook = r.json()
   print(orderbook['ok'])
+  return orderbook
 
-print(stocks('TESTEX'))
+def quote(venue, symbol):
+  r = requests.get('https://api.stockfighter.io/ob/api/venues/%s/stocks/%s/quote' % (venue, symbol))
+  quote = r.json()
+  print(quote['ok'])
+  return quote
+
+def placeOrder(order):
+  url = 'https://api.stockfighter.io/ob/api/venues/%s/stocks/%s/orders' % (order['venue'], order['stock'])
+  headers = {
+      'Content-Type': 'application/json',
+      'X-Starfighter-Authorization': apikey
+  }
+  r = requests.post(url, headers=headers, data=order)
+  confirmation = r.json()
+  print(confirmation['ok'])
+  return confirmation
+
+sampleOrder = {
+  'account': 'MST92145671',
+  'venue': 'LOBHEX',
+  'stock': 'LPEI',
+  'qty': 100,
+  'direction': 'buy',
+  'orderType': 'market'
+}
+
+placeOrder(sampleOrder)
