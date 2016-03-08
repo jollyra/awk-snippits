bf51bee66cfd220934cb95f28cdad14543bee8b0
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2016-03-07 23:22:23 -0800

    Get the orderbook for a venue and symbol

--- stockfighter.py
+++ stockfighter.py
@@ -7,6 +7,13 @@ def heartbeat(venue):
 
 def stocks(venue):
   r = requests.get('https://api.stockfighter.io/ob/api/venues/%s/stocks' % venue)
-  return r.json()
+  stocks = r.json()
+  print(stocks['ok'])
+  return stocks
+
+def orderbook(venue, symbol):
+  r = requests.get('https://api.stockfighter.io/ob/api/venues/%s/stocks/%s' % (venue, symbol))
+  orderbook = r.json()
+  print(orderbook['ok'])
 
 print(stocks('TESTEX'))
