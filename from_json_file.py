97813108cd27f85fa97c195f88756edec55399bd
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2016-03-11 00:02:44 -0800

    Algorithm description and capture market state

--- from_json_file.py
+++ from_json_file.py
@@ -1,9 +1,6 @@
 import json
-from pprint import pprint
 
 def load(fn):
   with open(fn, encoding='utf-8') as f:
     data = json.load(f)
     return data
-
-pprint(load('orderbook.json'))
--- market_maker.py
+++ market_maker.py
@@ -7,8 +7,25 @@ stock ='BWM'
 
 shares = 0  # must be between -1000 and +1000
 net_asset_value = 0  # goal is $10,000
-orders = []
+open_orders = []  # keep track of my position in the market
+spread = 10
+"""
+Algorithm:
+  init baseline
+  place asks at price p
+  if p is not filled:
+    place ask at price p + 1
+  else if p is filled:
+    place ask at p - 1
+    place bid at p - spread
+  update baseline
 
+Now in plain ambiguous English:
+  buy some stock as cheap as we can
+  try selling if for as much as possible
+  reduce price until we can sell
+  repeat
+"""
 def market_maker():
   while True:
     price = 0
@@ -25,24 +42,46 @@ def market_maker():
     else:
       price = 1
 
-    order = {
-      'account': account,
-      'venue': venue,
-      'stock': stock,
-      'qty': qty,
-      'price': price,
-      'direction': 'buy',
-      'orderType': 'limit'
-    }
-    client.place_order(order)
     count += 1
 
-def bootstrap_market_state(direction):
-  book = client.orderbook(venue, stock)
+
+# book = client.orderbook(venue, stock)
+def bootstrap_market_state(direction, orderbook):
   total_price = 0
   total_qty = 0
-  for bid in book[direction][:2]:
+  for bid in orderbook[direction][:2]:
     total_price += bid['price'] * bid['qty']
     total_qty += bid['qty']
   return total_price / total_qty
 
+def bid(qty, price):
+  order = {
+    'account': account,
+    'venue': venue,
+    'stock': stock,
+    'qty': qty,
+    'price': price,
+    'direction': 'buy',
+    'orderType': 'limit'
+  }
+  order_status = client.place_order(order)
+  if order_status['ok'] == 'true':
+    open_orders.append()
+  else:
+    print('Order invalid: %s' % order)
+
+def ask(qty, price):
+  order = {
+    'account': account,
+    'venue': venue,
+    'stock': stock,
+    'qty': qty,
+    'price': price,
+    'direction': 'ask',
+    'orderType': 'limit'
+  }
+  order_status = client.place_order(order)
+  if order_status['ok'] == 'true':
+    open_orders.append()
+  else:
+    print('Order invalid: %s' % order)
