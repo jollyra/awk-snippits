02d6013eb29f382a0420bfb6a3a2f261dcdf4442
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2016-03-17 20:29:28 -0700

    Fake some order data

---
+++ test_data.py
@@ -0,0 +1,82 @@
+test_orders = [{
+  "ok": "true",
+  "symbol": "FOOBAR",
+  "venue": "TESTEX",
+  "direction": "buy",
+  "originalQty": 100,
+  "qty": 100,
+  "price": 0,
+  "orderType": "limit",
+  "id": 1,
+  "account": "EXB123456",
+  "ts": "2016-03-16T06:02:07.755758288Z",
+  "fills": [{
+    "price": 5050,
+    "qty": 50,
+    "ts": "2015-07-05T22:16:18+00:00"
+    },{
+    "price": 5050,
+    "qty": 50,
+    "ts": "2015-07-05T22:16:18+00:00"
+    }
+  ],
+  "totalFilled": 0,
+  "open": "true"
+},{
+  "ok": "true",
+  "symbol": "FOOBAR",
+  "venue": "TESTEX",
+  "direction": "buy",
+  "originalQty": 100,
+  "qty": 100,
+  "price": 0,
+  "orderType": "limit",
+  "id": 2,
+  "account": "EXB123456",
+  "ts": "2016-03-16T06:02:07.755758288Z",
+  "fills": [{
+    "price": 5050,
+    "qty": 50,
+    "ts": "2015-07-05T22:16:18+00:00"
+    }],
+  "totalFilled": 0,
+  "open": "true"
+},{
+  "ok": "true",
+  "symbol": "FOOBAR",
+  "venue": "TESTEX",
+  "direction": "buy",
+  "originalQty": 100,
+  "qty": 100,
+  "price": 0,
+  "orderType": "limit",
+  "id": 3,
+  "account": "EXB123456",
+  "ts": "2016-03-16T06:02:07.755758288Z",
+  "fills": [{
+    "price": 5050,
+    "qty": 50,
+    "ts": "2015-07-05T22:16:18+00:00"
+    }],
+  "totalFilled": 0,
+  "open": "true"
+},{
+  "ok": "true",
+  "symbol": "FOOBAR",
+  "venue": "TESTEX",
+  "direction": "buy",
+  "originalQty": 100,
+  "qty": 100,
+  "price": 0,
+  "orderType": "limit",
+  "id": 4,
+  "account": "EXB123456",
+  "ts": "2016-03-16T06:02:07.755758288Z",
+  "fills": [{
+    "price": 5050,
+    "qty": 50,
+    "ts": "2015-07-05T22:16:18+00:00"
+    }],
+  "totalFilled": 0,
+  "open": "true"
+}]
