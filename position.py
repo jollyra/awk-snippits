415f8f1a6122df205ecd11e14b21f2e940712fbe
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2016-03-18 20:50:03 -0700

    Check the status of a list of open orders

--- position.py
+++ position.py
@@ -3,12 +3,18 @@ import client
 """
 Determine market position based off of filled orders.
 """
-def position(orders):
-  statuses = []
+
+
+
+"""
+Return an updated list of orders
+"""
+def update(self, orders):
+  checked_orders = []
   for order in orders:
     order_id = order['id']
     venue = order['venue']
     stock = order['symbol']
     order_status = client.order_status(order_id, venue, stock)
-    statuses.push(order_status)
-
+    checked_orders.push(order_status)
+  return checked_orders
